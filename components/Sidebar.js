99da976fbc956666f61bdc773c72d23580371244
jddadev <joethebartenderpdx@gmail.com>
2021-12-20 20:15:32 -0800

    installed next auth, created api/augh/[...nextauth].js

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -10,7 +10,7 @@ import {
 function Sidebar() {
   return (
     <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
-      <div>
+      <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
@@ -32,7 +32,7 @@ function Sidebar() {
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HeartIcon className="h-5 w-5" />
-          <p>Liked Songsi</p>
+          <p>Liked Songs</p>
         </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <RssIcon className="h-5 w-5" />
@@ -40,6 +40,23 @@ function Sidebar() {
         </button>
 
         <hr className="border-t-[0.1px] border-gray-900" />
+
+        {/* Playlists */}
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist Name...</p>
       </div>
     </div>
   );
