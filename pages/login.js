109902ff8a9feca1bcdd6c6f4968badb8f8138a9
jddadev <joethebartenderpdx@gmail.com>
2021-12-24 16:37:25 -0800

    debugging success, getting providers now in login.js

--- pages/login.js
+++ pages/login.js
@@ -9,8 +9,10 @@ function Login({ providers }) {
         alt="spotify logo"
       />
       {Object.values(providers).map((provider) => (
-        <div>
-          <button>test</button>
+        <div key={provider.name}>
+          <button className="bg-[#18D860] text-white p-5 rounded-full">
+            Login with {provider.name}
+          </button>
         </div>
       ))}
     </div>
@@ -22,7 +24,6 @@ export default Login;
 // Server side rendering, runs on server before the page gets delivered
 export async function getServerSideProps() {
   const providers = await getProviders();
-  console.log('PROVIDERS', providers);
   return {
     props: {
       providers,
