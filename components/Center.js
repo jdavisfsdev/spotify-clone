2a2a12eff2e66b4efd6a71a5e2a0e50820b42807
jddadev <joethebartenderpdx@gmail.com>
2021-12-28 23:19:38 -0800

    randomized header color with static color array using lodash shuffle

--- components/Center.js
+++ components/Center.js
@@ -1,8 +1,26 @@
 import { ChevronDownIcon } from '@heroicons/react/outline';
 import { useSession } from 'next-auth/react';
+import { useEffect, useState } from 'react';
+import { shuffle } from 'lodash';
+
+const colors = [
+  'from-indigo-500',
+  'from-blue-500',
+  'from-green-500',
+  'from-red-500',
+  'from-yellow-500',
+  'from-pink-500',
+  'from-purple-500',
+];
 
 function Center() {
   const { data: session } = useSession();
+  const [color, setColor] = useState(null);
+
+  useEffect(() => {
+    setColor(shuffle(colors).pop());
+  }, []);
+
   return (
     <div className="flex-grow">
       <header className="absolute top-5 right-8">
@@ -18,7 +36,7 @@ function Center() {
       </header>
 
       <section
-        className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8`}
       >
         {/* <img src="" alt="" /> */}
         hello
