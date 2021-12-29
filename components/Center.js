249d6849dac51d532cc59efcf32390b1cc5135da
jddadev <joethebartenderpdx@gmail.com>
2021-12-28 23:13:29 -0800

    Center header created, not done styling

--- components/Center.js
+++ components/Center.js
@@ -1,7 +1,28 @@
+import { ChevronDownIcon } from '@heroicons/react/outline';
+import { useSession } from 'next-auth/react';
+
 function Center() {
+  const { data: session } = useSession();
   return (
-    <div className="flex flex-grow text-white ">
-      <h1>THIS IS CENTER</h1>
+    <div className="flex-grow">
+      <header className="absolute top-5 right-8">
+        <div className="flex items-center space-x-3 opacity-90 hover:opacity-80 cursor-pointer rounded-full p-1 pr-2 bg-red-200">
+          <img
+            className="rounded-full w-10 h-10"
+            src={session?.user?.image}
+            alt=""
+          />
+          <h2>{session?.user?.name}</h2>
+          <ChevronDownIcon className="h-5 w-5" />
+        </div>
+      </header>
+
+      <section
+        className={`flex items-end space-x-7 bg-gradient-to-b to-black from-red-500 h-80 text-white p-8`}
+      >
+        {/* <img src="" alt="" /> */}
+        hello
+      </section>
     </div>
   );
 }
