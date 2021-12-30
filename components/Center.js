12ae2cc799088422c04fa212508ffbdeeb514567
jddadev <joethebartenderpdx@gmail.com>
2021-12-30 14:08:01 -0800

    useRecoilValue in Center to get playlistId, rerender center on playlistId change

--- components/Center.js
+++ components/Center.js
@@ -2,6 +2,8 @@ import { ChevronDownIcon } from '@heroicons/react/outline';
 import { useSession } from 'next-auth/react';
 import { useEffect, useState } from 'react';
 import { shuffle } from 'lodash';
+import { playlistIdState } from '../atoms/playlistAtom';
+import { useRecoilValue } from 'recoil';
 
 const colors = [
   'from-indigo-500',
@@ -16,10 +18,12 @@ const colors = [
 function Center() {
   const { data: session } = useSession();
   const [color, setColor] = useState(null);
+  //   read-only global state value
+  const playlistId = useRecoilValue(playlistIdState);
 
   useEffect(() => {
     setColor(shuffle(colors).pop());
-  }, []);
+  }, [playlistId]);
 
   return (
     <div className="flex-grow">
