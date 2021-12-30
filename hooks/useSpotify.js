3b19fcadb07d9dab2037d6b7f03a27601a3533cf
jddadev <joethebartenderpdx@gmail.com>
2021-12-30 13:45:33 -0800

    useSpotify hook to check session token, useEffect in Sidebar to getUserPlaylists and render thenm to the page

--- hooks/useSpotify.js
+++ hooks/useSpotify.js
@@ -1,5 +1,21 @@
+import { useSession, signIn } from 'next-auth/react';
+import { useEffect } from 'react';
+import spotifyApi from '../lib/spotify';
+
 function useSpotify() {
-  return <div></div>;
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    if (session) {
+      // if refresh access token attempt fails, redirect to login
+      if (session.error === 'RefreshAccessTokenError') {
+        signIn();
+      }
+      spotifyApi.setAccessToken(session.user.accessToken);
+    }
+  }, [session]);
+
+  return spotifyApi;
 }
 
 export default useSpotify;
